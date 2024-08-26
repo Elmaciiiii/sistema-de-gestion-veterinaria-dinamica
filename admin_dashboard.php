5725cc894c794583ac7b1df10b3c8283c1cb6d8c
byfrickxer <byfrickxer@gmail.com>
2024-08-26 19:04:01 -0300

    pagina admin

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -41,7 +41,7 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Veterinaria - Panel de Usuario</title>
+    <title>Veterinaria - Panel de Admin</title>
     <style>
         body {
             font-family: Arial, sans-serif;
